6f50b64dcf2e46339c0745e56a1f1b1104ee4eb3
Anh Nguyen <anhnguyen199819981998@gmail.com>
2022-04-16 01:38:09 -0700

    Add files via upload

--- final_project/8_view_manipulation.sql
+++ final_project/8_view_manipulation.sql
@@ -3,6 +3,11 @@ GO
 
 -- Update view
 ALTER VIEW HARDBACK_NOVELS_RELEASED_2022_04_13 AS
-SELECT novel_title
+SELECT novel_title, graphic_novel_id, novel_release_date
 FROM DC_COMICS.graphic_novel
-WHERE paperback_release = 0 and hardback_release = 1;
+WHERE paperback_release = 1 and hardback_release = 1
+GO
+
+-- Show view of novel 
+SELECT *
+FROM HARDBACK_NOVELS_RELEASED_2022_04_13
